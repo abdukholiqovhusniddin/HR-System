3212e56b3000b0719061c6289b62741790d077de
Husniddin <abdukholiqovh@gmail.com>
2025-10-01 09:34:46 +0500

    Fix bug

--- Application/Features/Contracts/Handlers/UpdateContractHandler.cs
+++ Application/Features/Contracts/Handlers/UpdateContractHandler.cs
@@ -1,12 +1,13 @@
 ﻿using Application.Commons;
 using Application.Exceptions;
 using Application.Features.Contracts.Commands;
+using Application.Interfaces;
 using Domain.Interfaces;
 using Mapster;
 using MediatR;
 
 namespace Application.Features.Contracts.Handlers;
-public class UpdateContractHandler(IContractsRepository contractsRepository)
+public class UpdateContractHandler(IContractsRepository contractsRepository, IFileService fileService)
     : IRequestHandler<UpdateContractCommon, ApiResponse<Contract>>
 {
     private readonly IContractsRepository _contractsRepository = contractsRepository;
@@ -18,6 +19,22 @@ public class UpdateContractHandler(IContractsRepository contractsRepository)
             ?? throw new NotFoundException("Contract not found");
 
         updateContract = contract.Adapt(updateContract);
+
+        if(contract.DocumentPdf is not null)
+        {
+            var documentPath = await fileService.SaveAsync(contract.DocumentPdf, "Contracts");
+            
+            updateContract.DocumentPdf = new ContractFile()
+            {
+                ContractId = updateContract.Id,
+                Name = documentPath.Name,
+                Url = documentPath.Url,
+                Size = documentPath.Size,
+                Extension = documentPath.Extension,
+            };
+            updateContract.DocumentUrl = documentPath.Url;
+        }
+        
 
         await _contractsRepository.UpdateAsync(updateContract);
 

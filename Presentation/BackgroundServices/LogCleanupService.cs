3da81f7ae84cf5500ab9965f0140eb4fbeb1639e
Husniddin <abdukholiqovh@gmail.com>
2025-10-08 06:35:28 +0500

    Added LogCleanupService

---
+++ Presentation/BackgroundServices/LogCleanupService.cs
@@ -0,0 +1,40 @@
+﻿namespace Presentation.BackgroundServices;
+
+public class LogCleanupService(ILogger<LogCleanupService> logger,
+    IWebHostEnvironment env) : BackgroundService
+{
+    private readonly ILogger<LogCleanupService> _logger = logger;
+    private readonly string _logsDirectory = Path.Combine(env.WebRootPath, "logs");
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                if (Directory.Exists(_logsDirectory))
+                {
+                    var files = Directory.GetFiles(_logsDirectory, "*.txt", SearchOption.AllDirectories);
+
+                    foreach (var file in files)
+                    {
+                        var creationTime = File.GetCreationTime(file);
+
+                        if (creationTime < DateTime.Now.AddMonths(-1))
+                        {
+                            File.Delete(file);
+                            _logger.LogInformation("Удалён старый лог: {file}", file);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при очистке логов");
+            }
+
+            // Запускаем проверку 1 раз в день
+            await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+        }
+    }
+}
--- Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a028bb9c0312568b4d24e1f737c3fcc03bd7a6bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6c08274c25c02c2baead502c7b596970c7ba90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

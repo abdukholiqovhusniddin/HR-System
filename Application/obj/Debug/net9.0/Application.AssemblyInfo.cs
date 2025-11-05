cee64b56410d50a9d63886a2eb2a3c1f99473728
Husniddin <abdukholiqovh@gmail.com>
2025-11-05 20:15:45 +0500

    Start work

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9691682494a44c3bc357eb823476788d013af7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3966c3d770e6b5bc8d12058b1cb089d0ba73727")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9691682494a44c3bc357eb823476788d013af7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3966c3d770e6b5bc8d12058b1cb089d0ba73727")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

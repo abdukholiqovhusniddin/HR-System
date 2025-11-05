1eb9a30f8a61eb736a4331ea32eb403b0b29d502
Husniddin <abdukholiqovh@gmail.com>
2025-11-05 23:02:28 +0500

    Added SendResetPassword

---
+++ Application/Features/Users/Handlers/SendResetPasswordCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commons;
+using Application.Features.Users.Commands;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.Users.Handlers;
+internal class SendResetPasswordCommandHandler(IUserRepository userRepository) 
+    : IRequestHandler<SendResetPasswordCommand, ApiResponse<string>>
+{
+    private readonly IUserRepository _userRepository = userRepository;
+    public Task<ApiResponse<string>> Handle(SendResetPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var requestDto = request.requestDto;
+        // Логика отправки письма для сброса пароля
+        // Например, генерация токена и отправка email
+        return Task.FromResult(new ApiResponse<string>
+        {
+            Data = "Reset password email sent successfully.",
+            StatusCode = 200
+        });
+    }
+}
--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3966c3d770e6b5bc8d12058b1cb089d0ba73727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee64b56410d50a9d63886a2eb2a3c1f99473728")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

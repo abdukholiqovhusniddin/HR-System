a8123f656cfba9cab05d805b221a3f27e9528796
Husniddin <abdukholiqovh@gmail.com>
2025-11-19 22:27:31 +0500

    feat: Added UpdateEmployeeCommandValidator

---
+++ Application/Features/Employees/Validators/UpdateEmployeeCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTOs.Employees.Requests;
+using FluentValidation;
+
+namespace Application.Features.Employees.Validators;
+
+public class UpdateEmployeeCommandValidator : AbstractValidator<UpdateEmployeeDtoRequest>
+{
+    private static readonly string[] AllowedExtensions = [".jpg", ".jpeg", ".png"];
+    private const long MaxFileBytes = 5 * 1024 * 1024; // 5 MB
+    public UpdateEmployeeCommandValidator()
+    {
+        When(x => x.Photo != null, () =>
+        {
+            RuleFor(x => x.Photo!.Length)
+                .LessThanOrEqualTo(MaxFileBytes)
+                .WithMessage("Photo size must be 5 MB or less.");
+
+            RuleFor(x => x.Photo!.FileName)
+                .Must(fn =>
+                {
+                    var ext = Path.GetExtension(fn)?.ToLowerInvariant();
+                    return ext != null && AllowedExtensions.Contains(ext);
+                })
+                .WithMessage("Photo must be a JPG or PNG file.");
+        });
+    }
+}
--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a60e28e41f00a6b3ea2cd10d454f75a26acaf34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529e8bd27485df11df5e8554efbbd6f436a41a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
